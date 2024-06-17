d633d8393aba5031ab724516d50f46ce1b2600ef
Xholi <90756178+Xholi@users.noreply.github.com>
2024-06-17 20:57:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
-import pyodbc
+# import pyodbc
 
 # Function definitions
 def validate_column_type(df, column_name, expected_type):
